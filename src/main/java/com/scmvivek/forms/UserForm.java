f013d49aca81281fbd1a7e5d0439c35bb3c4165a
moudekarvivek <moudekarvivek@gmail.com>
2024-05-08 14:45:08 +0530

    Validating User Signup Form using annotation in UserForm and Thymleaf

--- src/main/java/com/scmvivek/forms/UserForm.java
+++ src/main/java/com/scmvivek/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scmvivek.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,9 +18,21 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 // WE Will make the variables which we will recieve from the form
+    @NotBlank(message = "username is required")
+    @Size(message = "Min 3 Characters are required", min = 3)
     private String name;
+
+    @Email(message = "Invalid Email Address") // annotation for Validation of Email
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(message = "Min 6 character are required", min = 6)
     private String password;
+    
+    @NotBlank(message = "About is required")
     private String about;
+    
+    @Size(message = "Invalid Phone Number", min = 8, max = 12)
     private String phoneNumber;
 }
